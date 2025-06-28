6753867304aa48fb7e3e6ae612c6dc90d7a1afc6
Mauricio Rosales <maurirosrdz@gmail.com>
2025-06-28 13:55:05 -0600

    feat: add footer text

---
+++ client/src/components/Layout/CustomFooter.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-footer app padless>
+    <v-col class="text-center" cols="12" color="grey-lighten-4">
+      {{ footerText }}
+    </v-col>
+  </v-footer>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+
+const footerText = computed(() => {
+  return `©${new Date().getFullYear()} — Made with ❤️ in SLP 🌵 by TaG Team`
+})
+</script>
+
+<style scoped>
+
+</style>

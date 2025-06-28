454c0313db69ac3915deb864b68b2af74177510b
Mauricio Rosales <maurirosrdz@gmail.com>
2025-06-28 13:36:10 -0600

    feat: add project back-end and front-end

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue';
 import router from './router';
 import { createPinia } from 'pinia';
 import vuetify from './plugins/vuetify';
+import 'vuetify/styles';
+// import '@mdi/font/css/materialdesignicons.css';
 
 const app = createApp(App);
 app.use(router);

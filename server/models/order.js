f5cbcd73ce2c44bc3d64a47f196f639a6289571d
Carlos Mauricio Rosales Rodriguez <177691@upslp.edu.mx>
2025-07-01 19:25:07 -0600

    refactor: refactorize order model

--- server/models/order.js
+++ server/models/order.js
@@ -1,5 +1,5 @@
 export default (sequelize, DataTypes) => {
-  return sequelize.define('Order', {
+  const Order = sequelize.define('Order', {
     order_id: {
       type: DataTypes.INTEGER,
       primaryKey: true,
@@ -55,4 +55,41 @@ export default (sequelize, DataTypes) => {
     tableName: 't_order',
     timestamps: false
   });
+
+  Order.associate = (models) => {
+    Order.belongsTo(models.User, {
+      foreignKey: 'user_id',
+      as: 'user'
+    });
+  };
+
+  Order.associate = (models) => {
+    Order.belongsTo(models.OrderStatus, {
+      foreignKey: 'status_id',
+      as: 'status'
+    });
+  };
+
+  Order.associate = (models) => {
+    Order.belongsTo(models.ShippingType, {
+      foreignKey: 'shipping_id',
+      as: 'shipping_type'
+    });
+  };
+
+  Order.associate = (models) => {
+    Order.belongsTo(models.PaymentDetails, {
+      foreignKey: 'payment_id',
+      as: 'payment'
+    });
+  };
+
+  Order.associate = (models) => {
+    Order.belongsTo(models.Warehouse, {
+      foreignKey: 'warehouse_id',
+      as: 'warehouse'
+    });
+  };
+
+  return Order;
 };

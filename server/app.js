14de1bad914b09a2feb438d355fb22f9519d0a90
Mauricio Rosales <maurirosrdz@gmail.com>
2025-07-06 17:20:57 -0600

    feat: add missing endpoint routes

--- server/app.js
+++ server/app.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { sequelize } from './config/database.js';
-import ProductModel from './models/product.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
+import cartRoutes from './routes/cartRoutes.js';
+import cartDetailsRoutes from './routes/cartDetailsRoutes.js';
+import categoryRoutes from './routes/categoryRoutes.js'
+import orderRoutes from './routes/orderRoutes.js';
+import orderHistoryRoutes from './routes/orderHistoryRoutes.js';
+import orderStatusRoutes from './routes/orderStatusRoutes.js';
+import paymentRoutes from './routes/paymentRoutes.js';
+import shippingRoutes from './routes/shippingRoutes.js';
+import warehouseRoutes from './routes/warehouseRoutes.js';
 
 dotenv.config();
 const app = express();
@@ -12,13 +19,16 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const Product = ProductModel(sequelize, sequelize.constructor.DataTypes);
-app.use((req, res, next) => {
-  req.models = { Product };
-  next();
-});
-
+app.use('/api/cart', cartRoutes);
+app.use('/api/cart-details', cartDetailsRoutes);
+app.use('/api/category', categoryRoutes);
+app.use('/api/order', orderRoutes);
+app.use('/api/order-history', orderHistoryRoutes);
+app.use('/api/order-status', orderStatusRoutes);
+app.use('/api/payment', paymentRoutes);
 app.use('/api/products', productRoutes);
-app.use('/api', userRoutes);
+app.use('/api/shipping', shippingRoutes);
+app.use('/api/user', userRoutes);
+app.use('/api/warehouse', warehouseRoutes);
 
 export default app;

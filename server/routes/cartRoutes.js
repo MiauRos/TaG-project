57653d8f869a84032e6f0e9c6c501f0e029f3374
Mauricio Rosales <maurirosrdz@gmail.com>
2025-07-06 15:41:36 -0600

    feat: add cart endpoint routes

---
+++ server/routes/cartRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { createCart, getAllCarts, getCartById, updateCart, deleteCart } from '../controllers/cartController.js'
+
+const router = express.Router();
+
+router.post('/', createCart);
+router.put('/:id', updateCart);
+router.get('/', getAllCarts);
+router.get('/:id', getCartById);
+router.delete('/:id', deleteCart);
+
+export default router;

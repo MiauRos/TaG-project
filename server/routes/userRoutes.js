424eea8eef23f4115ef8c5ea9a56f71dc91216d6
Mauricio Rosales <maurirosrdz@gmail.com>
2025-07-06 15:47:31 -0600

    feat: add user endpoint routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,13 @@
 import express from 'express';
-import { loginUser } from '../controllers/userController.js';
+import { loginUser, createUser, getAllUsers, updateUser, deleteUser, getUserById } from '../controllers/userController.js';
 
 const router = express.Router();
 
 router.post('/login', loginUser);
+router.post('/', createUser);
+router.get('/', getAllUsers);
+router.get('/:id', getUserById);
+router.put('/:id', updateUser);
+router.delete('/:id', deleteUser);
 
 export default router;

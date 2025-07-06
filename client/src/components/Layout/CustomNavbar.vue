5291d1c6db6e60abf8d361bd4b03e44b90162f6b
Mauricio Rosales <maurirosrdz@gmail.com>
2025-07-06 12:15:54 -0600

    feat: adjust navbar details based on user logged

--- client/src/components/Layout/CustomNavbar.vue
+++ client/src/components/Layout/CustomNavbar.vue
@@ -3,14 +3,25 @@
     <v-toolbar-title>Tech and Gadgets</v-toolbar-title>
     <v-spacer />
     <v-btn text to="/">Inicio</v-btn>
-    <v-btn text to="/selection">Productos</v-btn>
-    <v-btn text to="/login">Login</v-btn>
+    <v-btn text to="/products">Productos</v-btn>
+    <v-btn text to="/login" v-if="!isLoggedIn">Login</v-btn>
+    <v-btn text @click="logout" v-else>Logout</v-btn>
   </v-app-bar>
 </template>
 
 <script setup>
+import { useUserStore } from '@/stores/user';
+import { useRouter } from 'vue-router';
+import { computed } from 'vue';
+
+const router = useRouter();
+const userStore = useUserStore();
+
+const isLoggedIn = computed(() => userStore.isLoggedIn);
+
+const logout = () => {
+  userStore.logout()
+  router.push('/login')
+};
 </script>
 
-<style scoped>
-
-</style>

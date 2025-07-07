7b905a208b4db389b79ba7f08c9f614783b073b0
Mauricio Rosales <maurirosrdz@gmail.com>
2025-07-06 19:50:58 -0600

    feat: add frontend routes

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,12 +3,14 @@ import Home from '../pages/Home.vue';
 import Login from '../pages/Login.vue'
 import Products from '../pages/Products.vue'
 import Register from '../pages/Register.vue'
+import Cart from '../pages/Cart.vue'
 
 const routes = [
   { path: '/', component: Home },
   { path: '/login', name: 'Login', component: Login },
   { path: '/products', name: 'Products', component: Products },
-  { path: '/register', name: 'Register', component: Register }
+  { path: '/register', name: 'Register', component: Register },
+  { path: '/cart', name: 'Cart', component: Cart }
 ];
 
 const router = createRouter({
